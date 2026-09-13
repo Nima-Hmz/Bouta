597c56fe9321e1f21dff6988deb6c5bd117904cc
Nima-Hmz <hmznima77@gmail.com>
2026-09-13 17:58:25 +0330

    fixing some comments

--- nursing_system/search_engine/core.py
+++ nursing_system/search_engine/core.py
@@ -3,7 +3,7 @@ from .filter import find_nearby_nurses
 def search_core(location_form, selected_skill_instance):
     # the core of the search engine
     radius_10km = 10 / 111.0  # ~0.09 degrees (~10 km)
-    radius_50km = 50 / 111.0  # ~0.09 degrees (~10 km)
+    radius_50km = 50 / 111.0  # ~0.09 degrees (~50 km)
     nurse_limit = 10
 
     result = find_nearby_nurses(location_form=location_form, radius=radius_10km, \
